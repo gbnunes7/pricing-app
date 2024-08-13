f9783b7d4e0370026aeea68fd2df6e1c74a73668
Gabriel Melo <bielrj170@gmail.com>
2024-08-12 23:18:15 -0300

    Add submit button to form with submit props

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,8 +1,8 @@
-const Form = () => {
+const Form = ({submit}) => {
     return(
         <form className="bg-gray-600 flex flex-col p-6 m-3.5 gap-2 rounded shadow-md md:w-2/4 lg:w-1/4">
             <label className="text-xl text-white" htmlFor="nomeMercadoria">Nome da Mercadoria</label>
-            <input className="border border-gray-300 p-1 rounded-md text-gray-700 placeholder-gray-500" type="text" id="nomeMercadoria" required placeholder="Ex: Tecido Branco" name="nomeMercadoria" pattern="^\d+(\.\d+)?$"/>
+            <input className="border border-gray-300 p-1 rounded-md text-gray-700 placeholder-gray-500" type="text" id="nomeMercadoria" required placeholder="Ex: Tecido Branco" name="nomeMercadoria" minLength="3" maxLength="120" pattern="^[A-Za-z\s]+$"/>
             
             <label className="text-xl text-white" htmlFor="custoMercadoria">Custo de Mercadoria</label>
             <input className="border border-gray-300 p-1 rounded-md text-gray-700 placeholder-gray-500" type="text" id="custoMercadoria" required placeholder="Ex: 2.99" name="custoMercadoria" pattern="^\d+(\.\d+)?$"/>
@@ -22,6 +22,7 @@ const Form = () => {
             <label className="text-xl text-white" htmlFor="lucroDesejado">Lucro desejado sobre vendas</label>
             <input className="border border-gray-300 p-1 rounded-md text-gray-700 placeholder-gray-500" type="text" id="lucroDesejado" name="lucroDesejado" required placeholder="Ex: 2 (porcento)" pattern="^\d+(\.\d+)?$"/>
             
+            <button onSubmit={submit}className="bg-blue-500 text-white py-2 px-4 mt-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">Confirmar</button>
         </form>
     )
 }
